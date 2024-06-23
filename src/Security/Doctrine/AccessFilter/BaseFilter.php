ba9c3005c559a9d0edcc4c86106c8d572db7f1da
Kenny1911 <o-muzyka@mail.ru>
2024-06-24 00:19:38 +0400

    feat: Add helper methods to BaseFilter
    Added methods is wrappers of same-name methods of UniqueNamer util:
    - BaseFilter::uniqueParamName()
    - BaseFilter::uniqueAlias()

--- src/Security/Doctrine/AccessFilter/BaseFilter.php
+++ src/Security/Doctrine/AccessFilter/BaseFilter.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Kenny1911\SymfonySecurityDoctrineFilterBundle\Security\Doctrine\AccessFilter;
 
+use Kenny1911\SymfonySecurityDoctrineFilterBundle\Security\Doctrine\Util\UniqueNamer;
+
 abstract class BaseFilter implements Filter
 {
     public function apply(string $attribute, ReadableQueryBuilder $qb, FilterSubject $subject, mixed $user): array
@@ -25,5 +27,15 @@ abstract class BaseFilter implements Filter
     protected function createConditionBuilder(): FilterConditionBuilder
     {
         return new FilterConditionBuilder();
+    }
+
+    protected function uniqueParamName(ReadableQueryBuilder $qb, int|string $name): string
+    {
+        return UniqueNamer::uniqueParamName($qb, $name);
+    }
+
+    protected function uniqueAlias(ReadableQueryBuilder $qb, string $alias): string
+    {
+        return UniqueNamer::uniqueAlias($qb, $alias);
     }
 }
